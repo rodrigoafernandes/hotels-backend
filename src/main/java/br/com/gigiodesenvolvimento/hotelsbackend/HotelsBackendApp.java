c958342cd6aa0e8a88f5cf22a2d05dc469c57e52
Rodrigo Fernandes <rodrigoforit@gmail.com>
2020-09-07 19:11:34 -0300

    Adicionado Gradle e Consul Config

--- src/main/java/br/com/gigiodesenvolvimento/hotelsbackend/HotelsBackendApp.java
+++ src/main/java/br/com/gigiodesenvolvimento/hotelsbackend/HotelsBackendApp.java
@@ -5,7 +5,7 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 
 import javax.ws.rs.core.Application;
 
-@OpenAPIDefinition(info = @Info(title = "Hotels-Backend", version = "0.0.1", description = "API para estimatima de " +
+@OpenAPIDefinition(info = @Info(title = "Hotels-Backend", version = "2.0.0", description = "API para estimativa de " +
         "preço de hoteis de acordo com a cidade informada"))
 public class HotelsBackendApp extends Application {
 
